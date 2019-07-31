ace54fd3cdef39bf4b892bb0789b8aeedf7859dc
Breno Oliveira dos Santos <breno@MacBook-Pro-de-Breno.local>
2019-07-30 22:56:24 -0300

    enviando email de cancelamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '3356c2f1a6899c',
+        pass: '45c7d79c5e481e',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
