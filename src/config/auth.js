aceed4995a56b54415be0f8ac9f0ce1874ddffad
Breno Oliveira dos Santos <breno@MacBook-Pro-de-Breno.local>
2019-07-31 23:56:20 -0300

    aidcionado .env

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-    secret: '87fde9de659ab2d503600223271ef9df',
+    secret: process.env.APP_SECRET,
     expiresIn: '7d',
 };
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-    dsn: 'https://5d368a409d58483b86fa0169173a72f9@sentry.io/1518836',
+    dsn: process.env.SENTRY_DSN,
 };
